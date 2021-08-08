416b9f334e0bee0578bd0dc2200682cbc4408185
Jisoo Choi <fyimbtmn@gmail.com>
2021-08-08 12:15:07 +0900

    Controller parameters

--- jex01/src/main/java/org/zerock/controller/SampleController.java
+++ jex01/src/main/java/org/zerock/controller/SampleController.java
@@ -2,16 +2,33 @@ package org.zerock.controller;
 
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
+
+
+
     @RequestMapping("")
     public void basic(){
         log.info("basic ..............");
@@ -30,5 +47,37 @@ public class SampleController {
         log.info("" + dto);
 
         return "ex01";
+    }
+
+    @GetMapping("/ex02")
+    public String ex02(String name, int age){
+        log.info("name : " + name);
+        log.info("age : " + age);
+        return "ex02";
+    }
+
+    @GetMapping("/ex02List")
+    public String ex02List(@RequestParam ArrayList<String> ids){
+        log.info("ids : " + ids);
+
+        return "ex02List";
+    }
+    @GetMapping("/ex02Array")
+    public String ex02Array(@RequestParam String[] ids){
+        log.info("ids : " + Arrays.toString(ids));
+
+        return "ex02Array";
+    }
+
+    @GetMapping("/ex02Bean")
+    public String ex02Bean(SampleDTOList list){
+        log.info("list dto : " + list);
+        return "ex02Bean";
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todo){
+        log.info("todo : " + todo);
+        return "ex03";
     }
 }
