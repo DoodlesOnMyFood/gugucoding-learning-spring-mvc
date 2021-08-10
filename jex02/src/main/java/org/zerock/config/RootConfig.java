4a8739e92662be78fb1dda8bacfe579eb1df4bb4
Jisoo Choi <fyimbtmn@gmail.com>
2021-08-10 12:37:18 +0900

    Chapter 9 completed. Added BoardService and corresponding tests

--- jex02/src/main/java/org/zerock/config/RootConfig.java
+++ jex02/src/main/java/org/zerock/config/RootConfig.java
@@ -7,12 +7,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("org.zerock.mapper")
+@ComponentScan(basePackages = {"org.zerock.service"})
 public class RootConfig {
 
     @Bean
---
+++ jex02/src/test/java/org/zerock/service/BoardServiceTest.java
@@ -0,0 +1,64 @@
+package org.zerock.service;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.config.RootConfig;
+import org.zerock.domain.BoardVO;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = RootConfig.class)
+@Log4j
+public class BoardServiceTest {
+    @Setter(onMethod_ = @Autowired)
+    BoardService service;
+
+    @Test
+    public void testExist(){
+        log.info(service);
+        assertNotNull(service);
+    }
+
+    @Test
+    public void testInsert(){
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 입력하는 테스트");
+        board.setContent("새로 입력하는 테스트 내용");
+        board.setWriter("newbie");
+
+        service.register(board);
+        log.info("생성된 게시물 번호 : " + board.getBno());
+    }
+
+    @Test
+    public void testGet() {
+        log.info(service.get(5L));
+    }
+
+    @Test
+    public void testGetList() {
+        service.getList().forEach(log::info);
+    }
+
+    @Test
+    public void testDelete() {
+        log.info("Remove Result : " + service.remove(2L));
+    }
+
+    @Test
+    public void testUpdate() {
+        BoardVO boardVO = service.get(6L);
+
+        if (boardVO == null) return;
+
+        boardVO.setTitle("수정 제목");
+        log.info("Modify result : " + service.modify(boardVO));
+    }
+}
